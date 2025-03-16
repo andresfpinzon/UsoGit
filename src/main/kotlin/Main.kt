41999fbc3dcb4b6094fb376b19f96c7d2cd94341
Andres Pinzon <andrespinzonxd@gmail.com>
2025-03-16 10:14:18 -0500

    feat: nuevos print

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,4 +3,8 @@ package org.example
 fun main() {
     println("Hello kotlin!")
     println("Hello kotlin!")
+    println("Texto diferente!")
+    println("Texto diferente!")
+    println("Texto diferente!")
+    println("Texto diferente!")
 }
